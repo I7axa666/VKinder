e84919ddda258b47a23c2c471b71b3ff0f21b481
Nikolay Zolotuhin <nik.ward@yandex.ru>
2023-03-18 00:35:42 +0300

    Fixed errors and add function get_project_root

--- modules/utils.py
+++ modules/utils.py
@@ -1,11 +1,14 @@
 import configparser
-import os.path
+from pathlib import Path
+
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent
 
 
 def get_data():
     config = configparser.ConfigParser()
-    dirname = os.path.dirname(__file__)
-    path = dirname + "/setting.ini"
+    path = str(get_project_root()) + "/setting.ini"
     config.read(path)
     name_user = config["DATABASE"]["USER"]
     password = config["DATABASE"]["PASSWORD"]
@@ -13,3 +16,6 @@ def get_data():
     token_vk = config["DATABASE"]["TOKEN_VK"]
     new_token_vk = config["DATABASE"]["VK_NEW_TOKEN"]
     return name_user, password, name_db, token_vk, new_token_vk
+
+
+get_data()
